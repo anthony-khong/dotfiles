34d2f23a4e5d709718492b477babe866b9ac40ac
akkhong <anthony.kusumo.khong@gmail.com>
2015-10-24 11:17:41 +0700

    Closer to a more complete setup.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,11 +1,63 @@
 # TODO: install git, install tmux, install vim
 # TODO: install all other stuff from my_setup.md
-cd;
 
+# Install essentials
+cd;
 if [ "$(uname)" == "Darwin" ]; then
     echo 'Setting up Mac...';
+    # TODO: Vim
+
+    # Xcode
+    xcode-select --install
+
+    # Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    brew install wget
+    brew install tmux
+
+    tlmgr install collection-fontsrecommended
+
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # TODO: Chrome, Dropbox, CUDA, Anaconda
     echo 'Setting up Linux...';
+
+    sudo apt-get update
+    sudo apt-get install build-essential
+    sudo apt-get install gnome-tweak-tool
+    sudo apt-get install compizconfig-settings-manager compiz-plugins-extra
+    sudo apt-get install htop
+    sudo apt-get install lm-sensors
+
+    # Git
+    sudo apt-get update
+    sudo apt-get install git
+
+    # Vim with clipboard
+    sudo apt-get update
+    sudo apt-get install vim-gtk
+
+    # Latest Tmux
+    sudo apt-get install -y python-software-properties software-properties-common
+    sudo add-apt-repository -y ppa:pi-rho/dev
+    sudo apt-get update
+    sudo apt-get install -y tmux=2.0-1~ppa1~t
+
+    # Pandoc
+    sudo apt-get install haskell-platform
+    cabal update
+    cabal install pandoc
+
+    # Spotify
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+    echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+    sudo apt-get update
+    sudo apt-get install spotify-client
+
+    # R
+    sudo apt-get update
+    sudo apt-get install r-base
+
 fi
 
 
@@ -24,17 +76,22 @@ echo 'Making dotfiles source from dotfiles_akk...';
 cd;
 
 rm .tmux.conf;
-echo "source-file ~/dotfiles_akk/tmux.conf" > .tmux.conf;
+echo "source-file ~/dotfiles_akk/tmux/tmux.conf" >> .tmux.conf;
 rm .vimrc;
 echo "set runtimepath^=~/dotfiles_akk/vim
-source ~/dotfiles_akk/vim/vimrc" > .vimrc;
+source ~/dotfiles_akk/vim/vimrc" >> .vimrc;
 
+cd;
 if [ "$(uname)" == "Darwin" ]; then
     rm .bash_profile
-    echo "source ~/dotfiles_akk/bash_profile" > .bash_profile
+    echo "source ~/dotfiles_akk/bash/bash_profile" >> .bash_profile
+    echo "source ~/dotfiles_akk/bash/bash_shortcuts" >> .bash_profile
+    echo "source ~/dotfiles_akk/bash/bash_preferences" >> .bash_profile
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     rm .bashrc
-    echo "source ~/dotfiles_akk/bashrc" > .bashrc
+    echo "source ~/dotfiles_akk/bash/bashrc" >> .bashrc
+    echo "source ~/dotfiles_akk/bash/bash_shortcuts" >> .bashrc
+    echo "source ~/dotfiles_akk/bash/bash_preferences" >> .bashrc
 fi
 
 

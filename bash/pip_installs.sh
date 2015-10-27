165eccfad2d7d709d99af108ead54f7e37658988
akkhong <anthony.kusumo.khong@gmail.com>
2015-10-27 23:01:04 +0700

    Worekd on bash

---
+++ bash/pip_installs.sh
@@ -0,0 +1,21 @@
+# Python
+pip install pdbpp
+
+# Machine Learning
+pip install Theano
+echo "[global]" >> .theanorc
+echo "floatX = float32" >> .theanorc
+
+
+conda install pydot
+pip install Lasagne
+pip install ggplot
+
+# Others
+pip install fuzzywuzzy
+pip install jedi
+pip install neovim
+pip install selenium
+pip install speedtest-cli
+pip install splinter
+pip install youtube-dl

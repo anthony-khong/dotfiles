67fa23c87a5ab01c6c7ba1da3758ea99c5d5d705
Anthony Khong <anthony.kusumo.khong@gmail.com>
2018-11-29 09:49:52 +0700

    .

--- google_cloud/startup.sh
+++ google_cloud/startup.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 export USER="akhong"
 export HOME="/home/akhong"
+export INSTALL_LOG="$HOME/.startup.log"
 
 sudo apt-get update && sudo apt-get install -y build-essential git
 
 cd $HOME && git clone https://github.com/anthony-khong/dotfiles.git
 
-export DONE_FILE="$HOME/.startup_done"
-if [[ ! -e "$DONE_FILE" ]]; then
-    /bin/bash dotfiles/google_cloud/startup_install.sh
-    touch $DONE_FILE
+if [[ ! -e "$INSTALL_LOG" ]]; then
+    /bin/bash $HOME/dotfiles/google_cloud/startup_install.sh
 fi

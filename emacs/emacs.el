33f57bb5396860ae87d27521809bfba6d6284884
Anthony Khong <anthony.kusumo.khong@gmail.com>
2019-02-07 19:51:30 +0700

    .

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -46,12 +46,6 @@
   :config
   (setq avy-keys '(?a ?o ?e ?u ?i ?d ?h ?t ?n ?s)))
 
-;; TODO
-;; (use-package evil-snipe :ensure t)
-;; (require 'evil-snipe)
-;; (evil-snipe-mode +1)
-;; (evil-snipe-override-mode +1)
-
 (use-package general :ensure t
   :config
   (general-evil-setup t)
@@ -100,6 +94,44 @@
    ;; Send (s)
    "ss" '(send-to-terminal-buffer :which-key "send to terminal buffer")))
 
+(use-package ivy :ensure t
+  :diminish (ivy-mode . "") ; does not display ivy in the modeline
+  :init (ivy-mode 1)        ; enable ivy globally at startup
+  :bind (:map ivy-mode-map  ; bind in the ivy buffer
+              ("C-'" . ivy-avy)) ; C-' to ivy-avy
+  :config
+  (setq ivy-use-virtual-buffers t)    ; extend searching to bookmarks and …
+  (setq ivy-height 20)                ; set height of the ivy window
+  (setq ivy-count-format "(%d/%d) ")) ; count format, from the ivy help page
+
+(use-package counsel :ensure t
+  :bind*                           ; load counsel when pressed
+  (("M-x"     . counsel-M-x)       ; M-x use counsel
+   ("C-x C-f" . counsel-find-file) ; C-x C-f use counsel-find-file
+   ("C-x C-r" . counsel-recentf)   ; search recently edited files
+   ("C-c f"   . counsel-git)       ; search for files in git repo
+   ("C-c s"   . counsel-git-grep)  ; search for regexp in git repo
+   ("C-c /"   . counsel-ag)        ; search for regexp in git repo using ag
+   ("C-c l"   . counsel-locate)))  ; search for files or else using locate
+
+(use-package which-key :ensure t
+  :init (which-key-mode)
+  :diminish (which-key-mode . ""))
+
+;; (use-package ido :ensure t
+;;   :config
+;;   (setq ido-enable-flex-matching t)
+;;   (setq ido-everywhere t)
+;;   (ido-mode 1)
+;;   (setq ido-create-new-buffer 'always))
+;; (require 'ido)
+
+;; TODO
+;; (use-package git-gutter :ensure t)
+;; (require 'git-gutter)
+;; (global-git-gutter-mode +1)
+
+
 (use-package xclip :ensure t)
 (xclip-mode 1)
 
@@ -119,27 +151,24 @@
 (require 'airline-themes)
 (airline-themes-set-modeline)
 
-;; (use-package doom-themes :ensure t)
-;; (require 'doom-themes)
-;; (setq doom-themes-enable-bold t
-;;       doom-themes-enable-italic t)
-;; (load-theme 'doom-one t)
 (use-package monokai-theme :ensure t)
 (load-theme 'monokai t)
 (set-background-color "unspecified-bg")
 (setq default-frame-alist '((background-color . "unspecified-bg")))
 
 (use-package company :ensure t
-  :init (company-mode))
+  :init (company-mode)
+  :diminish (company-mode . "")
+  :config
+  (global-company-mode 1)
+  (eval-after-load 'company
+    '(progn
+       (define-key company-active-map (kbd "TAB") 'company-complete-selection))))
 (require 'company)
-(global-company-mode 1)
-(eval-after-load 'company
-  '(progn
-     (define-key company-active-map (kbd "TAB") 'company-complete-selection)))
 
 (use-package volatile-highlights :ensure t
-  :config
-  (volatile-highlights-mode t))
+  :config (volatile-highlights-mode t)
+  :diminish (volatile-highlights-mode . ""))
 
 (use-package ranger :ensure t
   :commands (ranger)
@@ -157,49 +186,22 @@
 (use-package diminish :ensure t)
 (require 'diminish)
 
-(use-package ivy :ensure t
-  :diminish (ivy-mode . "") ; does not display ivy in the modeline
-  :init (ivy-mode 1)        ; enable ivy globally at startup
-  :bind (:map ivy-mode-map  ; bind in the ivy buffer
-         ("C-'" . ivy-avy)) ; C-' to ivy-avy
-  :config
-  (setq ivy-use-virtual-buffers t)    ; extend searching to bookmarks and …
-  (setq ivy-height 20)                ; set height of the ivy window
-  (setq ivy-count-format "(%d/%d) ")) ; count format, from the ivy help page
-
-(use-package counsel :ensure t
-  :bind*                           ; load counsel when pressed
-  (("M-x"     . counsel-M-x)       ; M-x use counsel
-   ("C-x C-f" . counsel-find-file) ; C-x C-f use counsel-find-file
-   ("C-x C-r" . counsel-recentf)   ; search recently edited files
-   ("C-c f"   . counsel-git)       ; search for files in git repo
-   ("C-c s"   . counsel-git-grep)  ; search for regexp in git repo
-   ("C-c /"   . counsel-ag)        ; search for regexp in git repo using ag
-   ("C-c l"   . counsel-locate)))  ; search for files or else using locate
-
-(use-package which-key :ensure t
-  :init (which-key-mode))
-
-;; TODO
-;; (use-package git-gutter :ensure t)
-;; (require 'git-gutter)
-;; (global-git-gutter-mode +1)
-
 ;;;; Emacs Lisp
 (use-package parinfer
   :ensure t
   :bind
   (("C-," . parinfer-toggle-mode))
+  :diminish (parinfer-mode . "")
   :init
   (progn
     (setq parinfer-extensions
           '(defaults       ; should be included.
-            pretty-parens  ; different paren styles for different modes.
-            evil           ; If you use Evil.
-            lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
-            paredit        ; Introduce some paredit commands.
-            smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
-            smart-yank))   ; Yank behavior depend on mode.
+             pretty-parens  ; different paren styles for different modes.
+             evil           ; If you use Evil.
+             lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+             paredit        ; Introduce some paredit commands.
+             smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+             smart-yank))   ; Yank behavior depend on mode.
     (add-hook 'clojure-mode-hook #'parinfer-mode)
     (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
     (add-hook 'common-lisp-mode-hook #'parinfer-mode)
@@ -343,7 +345,7 @@
  '(emamux:runner-pane-height 35)
  '(package-selected-packages
    (quote
-    (avy git-gutter evil-snipe rainbow-delimiters company eyebrowse anotehu evil-mode use-package evil-visual-mark-mode))))
+    (counsel evil-collection fzf avy git-gutter evil-snipe rainbow-delimiters company eyebrowse anotehu evil-mode use-package evil-visual-mark-mode))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

e3b507b6b3f04d5038ce4f2fa8d91505097a289d
Anthony Khong <anthony.kusumo.khong@gmail.com>
2019-11-01 12:51:34 +0700

    .

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,2 +1,3 @@
-{:user {:plugins [[org.clojure/clojure "1.10.0"]
-                  [cider/cider-nrepl "0.21.1"]]}}
+{:user {:plugins [[org.clojure/clojure "1.10.1"]
+                  [cider/cider-nrepl "0.21.1"]
+                  [lein-midje "3.2.1"]]}}

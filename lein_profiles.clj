fda643cbc4dd6d1adad4f1ed14edf1d48c7c22bb
Anthony Khong <anthony.kusumo.khong@gmail.com>
2019-12-09 12:49:28 +0700

    .

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[org.clojure/clojure "1.10.1"]
                   [cider/cider-nrepl "0.21.1"]
-                  [lein-midje "3.2.1"]]}
+                  [lein-midje "3.2.1"]
+                  [lein-cloverage "1.1.2"]]}
  :repl {:repl-options {:init (clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl
                                                                 :address "localhost"
                                                                 :port 55555

154b3c7abe7a9f36f45f1dd4c0e1e9f5358f279a
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-03-02 13:21:50 +0700

    .

--- google_cloud/startup_install.sh
+++ google_cloud/startup_install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-export USER="akhong"
-export HOME="/home/akhong"
+export USER="ubuntu"
+export HOME="/home/ubuntu"
 export INSTALL_LOG="$HOME/.startup.log"
 
 echo "Installing essential apps with apt-get..." >> $INSTALL_LOG

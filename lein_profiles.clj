750d67222e3c7085cee1b27cc538b44cf154c4fc
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-06-17 21:57:10 +0700

    .

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -1,11 +1,6 @@
-{:user {:plugins [[org.clojure/clojure "1.10.1"]
-                  [cider/cider-nrepl "0.21.1"]
-                  [lein-midje "3.2.1"]
-                  [lein-cloverage "1.1.2"]
+{:user {:plugins [[cider/cider-nrepl "0.25.2"]
                   [lein-ancient "0.6.15"]
-                  [cider/cider-nrepl "0.22.0"]
-                  [lein-cljfmt "0.6.6"]]}
- :repl {:repl-options {:init (clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl
-                                                                :address "localhost"
-                                                                :port 55555
-                                                                :name "lein"})}}}
+                  [lein-cljfmt "0.6.7"]
+                  [lein-cloverage "1.1.2"]
+                  [lein-midje "3.2.1"]
+                  [org.clojure/clojure "1.10.1"]]}}

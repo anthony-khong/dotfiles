4f97ef755f3939d9372db7a5372adc0942853614
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-08-22 12:50:21 +0700

    .

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -2,6 +2,7 @@
                   [lein-ancient "0.6.15"]
                   [lein-cljfmt "0.6.7"]
                   [lein-cloverage "1.1.2"]
-                  [lein-count "1.0.9"]
                   [lein-midje "3.2.1"]
-                  [org.clojure/clojure "1.10.1"]]}}
+                  [lein-count "1.0.9"]
+                  [org.clojure/clojure "1.10.1"]]
+        :signing {:gpg-key "D0A040786E09A660"}}}

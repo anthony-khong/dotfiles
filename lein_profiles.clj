045f76985d9ba78488ed6f6ccda9e92780be7699
Anthony Khong <anthony.kusumo.khong@gmail.com>
2020-08-22 13:53:41 +0700

    .

---
+++ lein_profiles.clj
@@ -0,0 +1,7 @@
+{:user {:plugins [[cider/cider-nrepl "0.25.2"]
+                  [lein-ancient "0.6.15"]
+                  [lein-cljfmt "0.6.7"]
+                  [lein-cloverage "1.1.2"]
+                  [lein-midje "3.2.1"]
+                  [lein-count "1.0.9"]
+                  [org.clojure/clojure "1.10.1"]]}}

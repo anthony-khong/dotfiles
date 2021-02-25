b52d4dce497cd36e91fcf9f41ceb5b2dc020fc86
anthony-khong <anthony.kusumo.khong@gmail.com>
2021-02-25 09:37:34 +0700

    Added gdrive-dl function

--- bash/bash_shortcuts.sh
+++ bash/bash_shortcuts.sh
@@ -196,3 +196,9 @@ function dec-tar () {
 function bb-nrepl () {
     bb --nrepl-server 4444
 }
+
+function gdrive-dl() {
+    # source: https://medium.com/@acpanjan/download-google-drive-files-using-wget-3c2c025a8b99
+    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$1' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$1" -O $2
+    rm -rf /tmp/cookies.txt
+}

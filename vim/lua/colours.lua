618bd11120d85a9572ece52681e0c4b23150ed72
Anthony Khong <anthony-khong@cridheil.local>
2026-09-24 23:42:24 +0700

    .

--- vim/lua/colours.lua
+++ vim/lua/colours.lua
@@ -1,5 +1,15 @@
 vim.opt.termguicolors = true
 
+local colors = require('ayu.colors')
+colors.generate()
+require('ayu').setup({
+  overrides = {
+    ['@module'] = { fg = colors.entity },
+    ['@string.special.symbol'] = { fg = colors.constant },
+    ['@punctuation.bracket'] = { fg = colors.fg },
+  },
+})
+
 vim.cmd([[
   colorscheme ayu-dark
   hi Pmenu guibg=#334351 guifg=#E6E1CF

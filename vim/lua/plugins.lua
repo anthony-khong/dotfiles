d8e105d15466facf70c8ed21bf08e8d1c69fa537
Anthony Khong <anthony-khong@cridheil.local>
2026-09-25 18:56:13 +0700

    .

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -1,91 +1,56 @@
--- Source: https://raw.githubusercontent.com/arusahni/dotfiles/fd6bf6435763f6d29e95ec3c42cdc33aa2cf6952/nvim/lua/plugins.lua
-
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap =
-    fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable",
+                  "https://github.com/folke/lazy.nvim.git", lazypath })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd [[packadd packer.nvim]]
+local lisps = { "clojure", "fennel", "janet", "hy", "racket", "scheme", "lisp" }
 
-return require("packer").startup {
-  function(use)
-    use "wbthomason/packer.nvim"
+require("lazy").setup({
+  spec = {
+    -- Loaded at startup
+    "nvim-lua/plenary.nvim",
+    { "shatur/neovim-ayu", priority = 1000 },
+    "lewis6991/gitsigns.nvim",
+    "lukas-reineke/indent-blankline.nvim",
+    "junegunn/vim-easy-align",
+    "tpope/vim-repeat",
+    "tpope/vim-surround",
+    "christoomey/vim-tmux-navigator",
+    "jpalardy/vim-slime",
+    "neovim/nvim-lspconfig",
+    "lukas-reineke/lsp-format.nvim",
+    { "nvim-treesitter/nvim-treesitter", branch = "main", build = ":TSUpdate" },
+    "j-hui/fidget.nvim",
+    "tpope/vim-projectionist",
+    "nvim-tree/nvim-tree.lua",
+    "nvim-telescope/telescope.nvim",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/vim-vsnip",
+    "hrsh7th/cmp-vsnip",
+    "rafamadriz/friendly-snippets",
+    { "elixir-tools/elixir-tools.nvim", tag = "stable" },
+    "mrcjkb/rustaceanvim",
+    "jidn/vim-dbml",
 
-    -- Utilities
-    use 'nvim-lua/plenary.nvim'
-
-    -- Appearance
-    use 'lewis6991/gitsigns.nvim'
-    use 'shatur/neovim-ayu'
-    use "lukas-reineke/indent-blankline.nvim"
-
-    -- Editing
-    use 'junegunn/vim-easy-align'
-    use 'tpope/vim-repeat'
-    use 'tpope/vim-surround'
-    use 'mattn/emmet-vim'
-
-    -- Tmux
-    use 'christoomey/vim-tmux-navigator'
-    use 'jpalardy/vim-slime'
-
-    -- Git
-    use 'tpope/vim-fugitive'
-
-    -- IDE
-    use 'neovim/nvim-lspconfig'
-    use "lukas-reineke/lsp-format.nvim"
-    use { "nvim-treesitter/nvim-treesitter", branch = "main", }
-    use 'j-hui/fidget.nvim'
-    use 'tpope/vim-projectionist'
-
-    use 'nvim-tree/nvim-tree.lua'
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = { {'nvim-lua/plenary.nvim'} }
-    }
-
-    -- Completion
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/nvim-cmp'
-
-    -- Snippets
-    use 'hrsh7th/vim-vsnip'
-    use 'hrsh7th/cmp-vsnip'
-    use "rafamadriz/friendly-snippets"
-
-    -- Elixir
-    use({ "elixir-tools/elixir-tools.nvim", tag = "stable", requires = { "nvim-lua/plenary.nvim" }})
-    use 'hrsh7th/cmp-nvim-lsp-signature-help'
-
-    -- Rust
-    use 'rust-lang/rust.vim'
-    use 'mrcjkb/rustaceanvim'
-
-    -- Lisp
-    use 'Olical/conjure'
-    use {
-      'eraserhd/parinfer-rust',
-      run = 'cargo build --release',
-      -- For M1 Macs:
-      -- $ cargo build --target=aarch64-apple-darwin --release
-      -- $ rm -rf target/release && mv target/aarch64-apple-darwin/release target/release
-    }
-    use 'hylang/vim-hy'
-    use 'guns/vim-sexp'
-    use 'tpope/vim-sexp-mappings-for-regular-people'
-
-    -- Others
-    use 'plasticboy/vim-markdown'
-    use 'jidn/vim-dbml'
-
-    if packer_bootstrap then
-      require("packer").sync()
-    end
-  end
-}
+    -- Loaded only when needed
+    { "tpope/vim-fugitive", cmd = { "G", "Git", "Gdiff", "Gvdiffsplit", "Gread", "Gwrite", "Gedit" } },
+    { "mattn/emmet-vim", ft = { "html", "heex", "eelixir", "css", "javascriptreact", "typescriptreact" } },
+    { "rust-lang/rust.vim", ft = "rust" },
+    { "plasticboy/vim-markdown", ft = "markdown" },
+    { "Olical/conjure", ft = lisps },
+    { "eraserhd/parinfer-rust", ft = lisps, build = "cargo build --release" },
+    { "hylang/vim-hy", ft = "hy" },
+    { "guns/vim-sexp", ft = lisps },
+    { "tpope/vim-sexp-mappings-for-regular-people", ft = lisps },
+  },
+  install = { colorscheme = { "ayu-dark" } },
+  rocks = { enabled = false },
+  change_detection = { notify = false },
+})
